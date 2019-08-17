f359cca78a463f75b94c28cc5ea144f02b5f0529
Albert Byrone <albertbyrone@1677.com>
2019-08-17 10:23:17 -0400

    This will create an if condition to check wether the date value is null ,empty and not a number

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 function validateForm (){
-    var day = document.getElementById("day").value;
+    var date = document.getElementById("day").value;
     var month = document.getElementById("month").value;
     var year = document.getElementById("year").value;
     var gender = document.getElementsByName("gender");
+
+    //validate the date 
+    if (date == "" || date == null) {
+
+        document.getElementById("inputError").innerHTML = "date is required";
+        document.getElementById("inputError").style.color = "red";
+        return false;
+
+    } else {
+        if (!isNaN(date)) {
+            if (date <= 0 || date > 31) {
+                document.getElementById("inputError").innerHTML = "date is Invalid";
+                document.getElementById("inputError").style.color = "red";
+                return false;
+            }
+        } else {
+            document.getElementById("inputError").innerHTML = "date is Empty";
+            document.getElementById("inputError").style.color = "red";
+            return false;
+        }
+    }
 };
