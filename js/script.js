9bbba6800048c3db7fe349be5707e755e9f112b3
Albert Byrone <albertbyrone@1677.com>
2019-08-17 10:20:10 -0400

    This will create variables day,month,year and gender respectively

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-function validateForm (){};
+function validateForm (){
+    var day = document.getElementById("day").value;
+    var month = document.getElementById("month").value;
+    var year = document.getElementById("year").value;
+    var gender = document.getElementsByName("gender");
+};
